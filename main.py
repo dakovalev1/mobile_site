a0413df2f4132e1e106ddb3fd49d8f29ca77cab2
Dmitry Kovalev <dakovalev1@mail.ru>
2019-04-01 17:59:42 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+index = open("docs/index.html", "w")
+
+
+def menu_item(item):
+    print("<li><a href=\"#" + item.lower() + ".body-section\" class=\"menu-item\" id=\"" + item.lower() + "\"><b>" + item + "</b></a></li>", file=index)
+
+
+print("<!DOCTYPE html>", file=index)
+print("<html lang = \"en-US\">", file=index)
+
+print("<head>", file=index)
+print("<meta charset=\"utf-8\">", file=index)
+print("<title>Author Name</title>", file=index)
+print("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">", file=index)
+print("<link rel=\"stylesheet\" href=\"main.css\">", file=index)
+print("</head>", file=index)
+
+print("<body>", file=index)
+# BODY BEGIN
+
+# MENU BEGIN
+print("<ul class=\"menu\">", file=index)
+print("<li><a href=\"index.html\" class=\"menu-title\"><b>Author Name</b></a></li>", file=index)
+#print("<li><a href=\"index.html\" class=\"menu-button\">X</a></li>", file=index)
+
+menu_item("Home")
+menu_item("News")
+menu_item("Papers")
+menu_item("Contact")
+
+print("</ul>", file=index)
+# MENU END
+
+
+# BODY END
+print("</body>", file=index)
+print("</html>", file=index)
+
+
+#print("", file=index)
+
+
+
